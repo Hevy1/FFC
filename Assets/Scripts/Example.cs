f8497e540692d6da147bd227cd9c6aee80debdf4
Hevy1 <evan.weisse@outlook.fr>
2024-10-19 12:19:26 +0200

    Added Map and Manager prefabs

--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Example : MonoBehaviour
 {
-    [SerializeReference] private Camera _camera;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float speed = 1.0f;
 
     // Start is called before the first frame update
     private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
