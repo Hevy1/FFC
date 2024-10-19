08c906342e63075fd1f48e4d719c0f85f11d2772
Hevy1 <evan.weisse@outlook.fr>
2024-10-19 23:44:05 +0200

    MOD: Player respawn and PlayerTrash spawn have the right rotation

---
+++ Assets/Scripts/Controllers/PlayerEnergy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerEnergy : MonoBehaviour
+{
+    [SerializeField] private float _maxEnergy = 200.0f;
+
+    private Slider _slider = null;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
